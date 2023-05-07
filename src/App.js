372798b0d35e84aa9bd1a4b24f5f6014633b68fb
crAy <crAyCoding@gmail.com>
2023-05-07 23:56:31 +0900

    PR 12. Use Class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Variable from './R010_Variable'
+import ClassPrototype from './R012_Class&Prototype'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <SpreadOperator/>
+      <ClassPrototype/>
     </div>
   );
 }
